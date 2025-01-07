c3fbcaf23d03de93dced4aa05f84e42d58ab6120
Lenka Marešová <lenkamaresu@gmail.com>
2025-01-07 21:07:19 +0100

    zacatek programovani hry

--- Hra.py
+++ Hra.py
@@ -1,16 +1,55 @@
 class Hra:
     def __init__(self):
         self.penize = 100
-        self.lokace = ['Hradčany', 'Václavák', 'Holešovice' ]
+        self.lokace = [Lokace('Hradčany'), Lokace('Václavák'), Lokace('Holešovice') ]
+        self.aktualni_lokace = self.lokace[0]
         self.dny = 0
         self.konec_hry = 14
 
     def presun_do_lokaci(self):
-        print('Dostupné lokace:1- Hradčany,2- Václavák,3- Holešovice')
+        print('Dostupné lokace:1-Hradčany,2- Václavák,3- Holešovice')
 
         nova_lokace = int(input('Kam chcete přejít?'))
+        if nova_lokace == 1:
+            self.aktualni_lokace = 'Hradčany'
+        elif nova_lokace == 2:
+            self.aktualni_lokace = 'Václavák'
+        elif nova_lokace == 3:
+            self.aktualni_lokace = 'Holešovice'
+
 
 
 
 
         self.dny += 1
+
+
+class Predmet:
+    def __init__(self, nazev, cena):
+        self.cena = cena
+        self.nazev = nazev
+
+
+class Lokace:
+    def __init__(self, misto):
+        self.misto = misto
+        self.predmety = [Predmet("Utopenec", 55), Predmet("Med", 100), Predmet("Láhev pálavy", 250)]
+
+
+
+
+class Osoba:
+    def __init__(self):
+        self.pocet_predmetu = 0
+        #self.penez = 100
+        self.kabat = False
+        self.batoh = False
+
+
+
+"""
+if __name__ ==  "__main__":
+    hradcany = Lokace() 
+    hra = Hra()
+    hra.presun_do_lokaci()
+"""
